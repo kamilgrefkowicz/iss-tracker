aa0cdfbb5868e7f0231fb08c6d7c540638b8a1db
kamilgrefkowicz <kamilgrefkowicz@gmail.com>
2021-07-12 19:20:25 +0200

    initial commit

---
+++ src/test/java/pl/kamil/isstracker/IssTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kamil.isstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
