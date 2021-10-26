9756bbc85002ecfa15d892721d1b46db72d33a25
kamilgrefkowicz <kamilgrefkowicz@gmail.com>
2021-10-26 13:39:22 +0200

    fix

--- frontend/iss-front/pretest.sh
+++
@@ -1,7 +0,0 @@
-echo "Running 'pretest.sh' script do download Google Maps API for testing..."
-# rm google-maps-api.js
-date +"// Download time: %Y-%m-%d %H:%M" >> google-maps-api.js
-curl "https://maps.googleapis.com/maps/api/js?key=AIzaSyAoJnPw89bM2cW5X4UNR4H1rJGv8AlSyrU&libraries=&v=weekly&language=en" >> google-maps-api.js
-echo "'pretest.sh' finished"
-
- 
